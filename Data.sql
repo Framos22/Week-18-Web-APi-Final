4d6ce608cb98703ab3a5686171e50f61391814c4
Felix Ramos <100186555+Framos22@users.noreply.github.com>
2022-06-25 19:09:41 -0700

    Add files via upload

---
+++ Data.sql
@@ -0,0 +1,31 @@
+INSERT INTO record_label (label_name, year_created) VALUES ('Death Row', '1991');
+INSERT INTO record_label (label_name, year_created) VALUES ('Def Jam', '1984');
+INSERT INTO record_label (label_name, year_created) VALUES ('OVO Records', '2012');
+INSERT INTO record_label (label_name, year_created) VALUES ('Interscope', '1990');
+INSERT INTO record_label (label_name, year_created) VALUES ('Aftermath', '1996');
+INSERT INTO artists (first_name, last_name) VALUES ('Snoop', 'Dogg');
+INSERT INTO artists (first_name, last_name) VALUES ('Kanye', 'West');
+INSERT INTO artists (first_name, last_name) VALUES ('Aubrey', 'Graham');
+INSERT INTO artists (first_name, last_name) VALUES ('Sean', 'Carter');
+INSERT INTO artists (first_name, last_name) VALUES ('Nayvadius', 'Wilburn');
+INSERT INTO songs (song) VALUES ('Drop It Like Its Hot');
+INSERT INTO songs (song) VALUES ('Otis');
+INSERT INTO songs (song) VALUES ('No Church In The Wild');
+INSERT INTO songs (song) VALUES ('Digital Dash');
+INSERT INTO songs (song) VALUES ('Big Rings');
+INSERT INTO songs (song) VALUES ('Digital Dash');
+INSERT INTO songs (song) VALUES ('Flashing Lights');
+INSERT INTO songs (song) VALUES ('One Dance');
+INSERT INTO songs (song) VALUES ('99 Problems');
+INSERT INTO songs (song) VALUES ('Mask Off');
+INSERT INTO albums (title, year_released, label, song_id, artists_id, record_label_id) VALUES ('Watch The Throne', '2011', 'GooD Music', '2', '2', '3');INSERT INTO albums (title, year_released, label, song_id, artists_id, record_label_id) 
+VALUES ('Watch The Throne', '2011', 'GOOD Music', '2', '2', '3');
+INSERT INTO albums (title, year_released, label, song_id, artists_id, record_label_id) VALUES ('What A Time To Be Alive', '2015', 
+'Young Money', '4', '3', '3');
+
+
+
+
+
+
+
